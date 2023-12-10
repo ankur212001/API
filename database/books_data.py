092aa44de097595e2163ce9999181ed737ef55a3
ankur212001 <87575898+ankur212001@users.noreply.github.com>
2023-12-10 21:44:01 +0530

    Update books_data.py

--- database/books_data.py
+++ database/books_data.py
@@ -13,22 +13,50 @@ def get_all_books(db):
 
 
 def post_book(new_book, db):
+    query_check_if_exists = f"""
+                                SELECT COUNT(isbn) 
+                                FROM books 
+                                WHERE isbn = :isbn;
+                            """
+
     query_insert_book = f"""
                             INSERT INTO books (isbn, title, subtitle, author, published, publisher, pages, description, website)
                             VALUES(:isbn, :title, :subtitle, :author, :published, :publisher, :pages, :description, :website)
                         """
     
+    b = db.execute(text(query_check_if_exists), {"isbn": new_book.isbn})\
+            .fetchall()\
+            [0][0]
+
+    if b == 1:
+        return "book already exists"
+    
     db.execute(text(query_insert_book), new_book.__dict__)
 
     db.commit()
 
+    return "book added successfully"
+
 
 def update_book(id, updated_book, db):
+    query_check_if_exists = f"""
+                                SELECT COUNT(id)
+                                FROM books
+                                WHERE id = :id
+                            """
+    
     query_update_book = f"""
                             UPDATE books
                             SET isbn = :isbn, title = :title, subtitle = :subtitle, author = :author, published = :published, publisher = :publisher, pages = :pages, description = :description, website = :website
                             WHERE id = {id}
                         """
+    
+    b = db.execute(text(query_check_if_exists), {"id": id})\
+            .fetchall()\
+            [0][0]
+
+    if b == 0:
+        return "no such book exists"
     
     db.execute(text(query_update_book), updated_book.__dict__)
 
