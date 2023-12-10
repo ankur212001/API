f6f711da81d37345cd33963d2b64155c9907c0a6
ankur212001 <87575898+ankur212001@users.noreply.github.com>
2023-12-10 21:43:44 +0530

    Update books.py

--- routers/books.py
+++ routers/books.py
@@ -13,7 +13,26 @@ router = APIRouter(
 @router.get('/', status_code=200)
 def get_all_books(db: Session = Depends(get_mysql_db)):
     try:
-        books_data.get_all_books(db)
+        books = books_data.get_all_books(db)
+        
+        data = []
+        val = {}
+        for book in books:
+            val["id"] = book[0]
+            val["isbn"] = book[1]
+            val["title"] = book[2]
+            val["subtitle"] = book[3]
+            val["author"] = book[4]
+            val["published"] = book[5]
+            val["publisher"] = book[6]
+            val["pages"] = book[7]
+            val["description"] = book[8]
+            val["website"] = book[9]
+
+            data.append(val)
+
+        return data
+
     
     except Exception as e:
         print(e)
@@ -22,16 +41,17 @@ def get_all_books(db: Session = Depends(get_mysql_db)):
 @router.post('/', status_code=200)
 def add_new_book(new_book: book, db: Session = Depends(get_mysql_db)):
     try:
-        books_data.post_book(new_book, db)
+        return books_data.post_book(new_book, db)
 
     except Exception as e:
         print(e)
+        return 
 
 
 @router.patch('/{id}', status_code=200)
 def update_book(id, updated_book: book, db: Session = Depends(get_mysql_db)):
     try:
-        books_data.update_book(id, updated_book, db)
+        return books_data.update_book(id, updated_book, db)
 
     except Exception as e:
         print(e)
